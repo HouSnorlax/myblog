db34863bb79a0cf2cde354b28a4ab40f8ab03d99
HouSnorlax <normakir0b7@gmail.com>
2026-01-16 10:07:05 +0900

    自己紹介を切り分け

---
+++ components/introduction.tsx
@@ -0,0 +1,14 @@
+export function Introduction () {
+    return (
+        <div class="p-4 border rounded text-xl">
+        <h1 class="text-2xl font-bold mb-1">自己紹介</h1>
+        つめありと申します<br/>
+        情報系学生やってます<br/>
+        趣味はライブ参戦(主に👇女性声優👆)、ウマ娘、麻雀、相撲観戦、競馬観戦etc...<br/>
+        推し→<span class="font-bold">小島　菜々恵様</span>、<span class="font-bold text-red-900">スティルインラブ(ウマ娘)</span><br/>
+        SNS等リンク: <a href="https://x.com/kabinoyama_beya" class="text-blue-700 font-bold hover:underline">X</a>&nbsp;
+        <a href="https://bsky.app/profile/housnorlax.bsky.social" class="text-blue-700 font-bold hover:underline">Bluesky</a>&nbsp;
+        <a href="https://www.youtube.com/@kabinoyama_beya" class="text-blue-700 font-bold hover:underline">YouTube</a>
+      </div>
+    );
+}
